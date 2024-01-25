eb5f3a06ae5f726a5de357d6d0ec622393569991
JackelineGS <jackeline.garcia.serna@gmail.com>
2024-01-24 22:18:00 -0500

    We add content in the home

--- src/components/home.js
+++ src/components/home.js
@@ -1,12 +1,17 @@
+
 const home = (navigateTo) => { 
 
     const homeDiv = document.createElement('div');
     homeDiv.className = 'homeDiv'; 
 
+    fetch('https://fakestoreapi.com/products')
+    .then(res=>res.json())
+    .then(json=>console.log(json))
+
     homeDiv.innerHTML = '';
     homeDiv.innerHTML += `
     <header>
-    <div class='logoDev'></div>
+    
         <div class='bar'>
             <button class='bHome'>Home</butoon>
             <button class='bNosotros'>Nosotros</butoon>
@@ -17,22 +22,37 @@ const home = (navigateTo) => {
     </header>
     <main class='homeMain'>
     <div class='content'>
-        <div class='texto'> 
-            <h1 class='titleUno'>HOLA, MI NOMBRE ES</h1>
-            <h1 class='titleDos'>JACKELINE GARCÍA</h1>
-            <h1 class='titleTres'>WEB DEVELOPER</h1> 
-            <p class='presentationHome'> Esta es mi página web. Siéntete libre de navegar y saber más sobre mí. </p>
-        </div>
-        <div class='homeImg'>
-            <div class='firstPicture'></div>
-            <div class='secondPicture'></div>
-            <div class='thirdPicture'></div>
-            <div class='fourPicture'></div>
-        </div>
     </div>
         
     </main>
     <footer>
+    <div class="container">
+    <div class="row">
+      <div class="col-md-6">
+        <h4>Información de contacto</h4>
+        <p>
+          Dirección: 123 Main Street, Anytown, CA 12345
+          <br>
+          Teléfono: (555) 555-5555
+          <br>
+          Correo electrónico: info@example.com
+        </p>
+      </div>
+      <div class="col-md-6">
+        <h4>Enlaces de interés</h4>
+        <ul>
+          <li><a href="#">Política de privacidad</a></li>
+          <li><a href="#">Términos de servicio</a></li>
+          <li><a href="#">Contacto</a></li>
+        </ul>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-md-12">
+        <p>Copyright &copy; 2023 Ejemplo</p>
+      </div>
+    </div>
+  </div>
     </footer> 
     `;
 
@@ -62,7 +82,53 @@ const home = (navigateTo) => {
     });
 
 
+    function crearTarjetas (productos) {
+        const all = homeDiv.querySelector('.content');
+        all.innerHTML = '';
+        productos.forEach(producto => {
+            all.innerHTML += `
+            <div class="card">
+                <div class="card-body-img">
+                    <img class="poster" src="${producto.image}" alt="Imagen de la película"/>
+                </div>
+                <div class="card-body-text">
+                    <h5 class="card-title">${producto.title}</h5>
+                    <span class="año">Año: ${producto.price}</span>
+                    <span class="score">Score: ${producto.category}</span>
+                    <button name="button" class="button-info" data-id="${producto.id}">Añadir al carrito</button>
+                </div>
+            </div>
+            `
+        });
+    }
+
+
+    async function fetchData() {
+        try {
+          const response = await fetch("https://fakestoreapi.com/products");
+          if (response.ok) {
+            const products = await response.json();
+            console.log(products);
+            crearTarjetas(products);
+          } else {
+            console.log(response.status);
+          }
+        } catch (error) {
+          console.log(error);
+        }
+      }
+      
+      fetchData();
+     
+   
+
+  
+
     return homeDiv;
+
+
 };
+
+
 
 export default home;
