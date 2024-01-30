df6c18588a9fe54239e3e89b49a50be4f72b3204
JackelineGS <jackeline.garcia.serna@gmail.com>
2024-01-29 23:16:44 -0500

    Agregamos los estilos

--- src/components/nosotros.js
+++ src/components/nosotros.js
@@ -16,28 +16,38 @@ const nosotros = (navigateTo) => {
     </div>
     </header>
     <main class='nosotrosMain'>
-        <div class='slogan'>
-            <p class='textoSlogan>
-                Encuentra todo desde tu tienda Online
-            </p>
-            <div class='imagenSlogan'></div>
-        </div>
-        </div>
-            <div class='aboutDescription'>
-                <div class='firstAbout'>
-                    <h1 class='firstTitle'>SOBRE MI</h1>
-                    <p class='pDescription'>Bienvenidos a mi página web, mi nombre es Jackeline García, 
-                    web developer front-end. Me apasiona participar en proyectos que permitan
-                    mejorar el bienestar de las personas, así mismo estoy muy interesada en el aprendizaje de nuevas
-                    tecnologías.
-                   </p>  
-                </div>
-                <div class='skillBar'>
-                    <h1 class='titleSkill'>SKILLS</h1>
-                    <div class='allSkills'>
-                      
+            <div class='nosotrosContent'>
+                <div class='presentacion'>
+                    <h2 class='encabezado'>SOBRE NOSOTROS</h2>
+                    <div class='slogan'>
+                    <p class='sloganText'> <span class='titleBrand'>StoreAll: </span> Encuentra moda y tecnología en un solo lugar</p>
+                    <div class='sloganLogo'></div> 
                     </div>
                 </div>
+                <div class='misionVision'>
+                    <h2 class='encabezado'>SOBRE NOSOTROS</h2>
+                    <div class='valores'>
+                        <div class='mision'>
+                            <div class='logoMision'></div>
+                            <div class='logoText'>
+                                <h2 class='titleMision'>MISION</h2>
+                                <p>Llevar la moda a todo el mundo, ofreciéndoles a nuestros clientes una amplia selección de productos de alta calidad a precios asequibles.</p>
+                            </div>
+                        </div>
+                    <div class='vision'>
+                        <div class='logoVision'></div>
+                        <div class='logoText'>
+                            <h2 class='titleVision'>VISION</h2>
+                            <p>Convertirnos en la tienda online de moda más grande del mundo, ofreciendo a nuestros clientes una experiencia de compra única.</p>
+                        </div>
+                    </div> 
+
+                    </div>
+                    
+                    
+                    </div>
+                </div>
+
             </div
         </div>
     </main>
