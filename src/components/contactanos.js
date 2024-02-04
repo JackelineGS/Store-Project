69d9e9bde7ab5e0ee758cb0b4618526d5f27ff91
JackelineGS <jackeline.garcia.serna@gmail.com>
2024-02-03 22:40:22 -0500

    We start with contact page

--- src/components/contactanos.js
+++ src/components/contactanos.js
@@ -16,20 +16,16 @@ const contactanos = (navigateTo) => {
     </div>
     </header>
     <main class='contactMain'>
-      <div class='dContact'>
-        <div class='divInfo'>
-          <p class='request'> Estoy buscando nuevas oportunidades de desarrollo de proyectos o colaboraciones.
-          Si tienes un proyecto en mente, estaré encantada de discutir tus necesidades 
-          y ver si puedo ayudarte a cumplir tus objetivos. Si te interesa comunicarte conmigo enviame un correo: 
-          </p>
-          <p class='correo'>jackeline.garcia.serna@gmail.com</p>
-          <a class='ancorLink' href='https://www.linkedin.com/in/jackeline-garcia-ramh/' target='_blank'  type='button'>LinkedIn</a>
-          <a class='ancorGit' href='https://github.com/JackelineGS' target='_blank' type='button'>GitHub</a>
+     <div class='dContact'>
+        <div class='divContactText'>
+          <h3 class='encabezadoContact'> Contáctanos </h3>
+          <p class='pregunta'>¿Tienes alguna pregunta, idea o proyecto en mente? </p>
+          <p class='pregunta_dos'> ¡Contáctanos y hagámoslo realidad juntos! </p>
+          <div class='imgContact'></div>
         </div>
-        <div class='contactImg'>
-        <div class='imgForest'></div>
+        <div class='formContact'></div>
         </div>
-        </div>
+     </div>
     </main>
     `; 
 
